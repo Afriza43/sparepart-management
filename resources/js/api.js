ec0d47f583d46e9ea37ca63d3d438eb0fc36a2d3
Afriza Andhana <meidioo2003@gmail.com>
2025-12-20 18:17:53 +0700

    fix api

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://sparepart-management.test/api/v1",
+    baseURL: "/api/v1",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
