0850e430805e0063e84b3eb1dac1cc5e5e09941c
Afriza Andhana <meidioo2003@gmail.com>
2026-01-18 18:49:17 +0700

    add logout function

--- resources/js/api.js
+++ resources/js/api.js
@@ -22,4 +22,25 @@ api.interceptors.request.use(
     },
 );
 
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            console.log("Sesi habis (401 detected). Melakukan auto-logout...");
+
+            localStorage.removeItem("token");
+
+            localStorage.removeItem("user");
+
+            if (window.location.pathname !== "/login") {
+                window.location.href = "/login";
+            }
+        }
+
+        return Promise.reject(error);
+    },
+);
+
 export default api;
